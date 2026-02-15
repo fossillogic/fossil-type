c0821df8901806637834a54f22811af59b58866a
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2026-02-15 16:40:22 -0600

    Create lambda.c

---
+++ code/logic/lambda.c
@@ -0,0 +1,44 @@
+/**
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop
+ * high-performance, cross-platform applications and libraries. The code
+ * contained herein is licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain
+ * a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ * Date: 04/05/2014
+ *
+ * Copyright (C) 2014-2025 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#include "fossil/type/lambda.h"
+
+/* ======================================================
+ * Construction
+ * ====================================================== */
+lambda fossil_type_lambda_make(fossil_type_lambda_fn fn, void* ctx) {
+    lambda l;
+    l.fn = fn;
+    l.context = ctx;
+    return l;
+}
+
+/* ======================================================
+ * Execution
+ * ====================================================== */
+void fossil_type_lambda_call(const lambda* l) {
+    if (!l || !l->fn)
+        return;
+    l->fn(l->context);
+}
